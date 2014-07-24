fd4d5f91801d56ef89cc308204c952924aca394d
Yusuke Sasajima <zimathon@gmail.com>
2014-07-24 15:15:44 +0900

    delete gitconfig

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,4 @@
 ln -s -f ~/dotfiles/.vimrc ~/.vimrc
 ln -s -f ~/dotfiles/.vim ~/.vim
 ln -s -f ~/dotfiles/.xvimrc ~/.xvimrc
-ln -s -f ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s -f ~/dotfiles/.zshrc ~/.zshrc
