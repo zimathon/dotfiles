848f50b2504a9b8b20c4980bdb7dfc9b09ee79d3
笹島 祐介 <sasajimay@love-steve-2.local>
2014-08-26 14:05:25 +0900

    add gitignore

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,3 +3,5 @@ ln -s -f ~/dotfiles/.vim ~/.vim
 ln -s -f ~/dotfiles/.xvimrc ~/.xvimrc
 ln -s -f ~/dotfiles/.zshrc ~/.zshrc
 ln -s -f ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s -f ~/dotfiles/.gitignore ~/.gitignore
+
